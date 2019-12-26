e793616b6900b6ef08e173c662285f45f33e8ff1
Tanuj Dey <tanujdey7@gmail.com>
2019-12-26 13:26:56 +0530

    v1.0

---
+++ 13.py
@@ -0,0 +1,4 @@
+num = 0
+for i in range(1,100):
+    num = num + i 
+print(num) 
---
+++ 14.py
@@ -0,0 +1,5 @@
+maths = int(input("Enter Marks of Maths: "))
+science = int(input("Enter Marks of Science: "))
+english = int(input("Enter Marks of English: "))
+total = maths+science+english
+print("Total: "+str(total))
